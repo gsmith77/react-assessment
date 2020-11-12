934f92cb16435e577f6b3c94d76bc97210a83a07
gsmith77 <grantsmith1256@gmai.com>
2020-11-11 22:35:38 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,66 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
+import React, { useState } from 'react';
 
 function App() {
+
+  const [inputValue, setInputValue] = useState('');
+  const [shows, setShows] = useState([]);
+
+
+  const fetchShows = async () => {
+    try {
+      const data = await axios.get(`https://api.tvmaze.com/search/shows?q=${inputValue}`)
+      console.log(data.data)
+      setShows(data.data)
+    } catch (e) {
+      alert(`${e.message}`)
+    }
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">Show Finder</div>
+      <div style={{height: shows.length ? "100%" : "100vh"}} className="border">
+        <div className="row input-icons">
+          <i class="fa fa-search icon"></i> 
+          <input className="search-input input-field" onChange={(e) => setInputValue(e.target.value)} placeholder="The Office" value={inputValue} type="text" />
+          <button className="search-button" onClick={fetchShows}>Search</button>
+        </div>
+        <main>
+          <div className="shows">
+            {shows.length ? shows.map(item => {
+              const { show } = item
+              let summary = show.summary ? show.summary.replace('<p>', '') : null
+              summary = summary ? summary.replace(/<[^>]+>/g, '') : 'No Summary'
+              return (
+                <div className="row clear show">
+                  <img src={show.image ? show.image.medium : 'https://house.utah.gov/wp-content/uploads/2019/01/Image-Coming-Soon-768x768.jpg'} alt="show" className="show-image column large-2" />
+                  <div className="show-background column large-7">
+                    <h1 className="show-text">{show.name}</h1>
+                    <div className="show-text">{summary}</div>
+                    <button className="show-episodes-button">Show Episodes</button>
+                  </div>
+                </div>
+              )
+            }) : <h1 style={{textAlign: 'center'}}>Go Search Some Shows!</h1>}
+          </div>
+        </main>
+
+      </div>
     </div>
   );
 }
 
 export default App;
+
+
+// Success is:
+// The initial page will have a text input and button.
+// Successfully make the api call
+// Display the results in a view that is human readable.
+
+
+// Tasks:
+// create input
+// create call upon click
